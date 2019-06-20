5f75c4274dcda6ca0fda6dd0ce4aaa2e7c9c112c
kimspractice <tpghksdldia@naver.com>
2019-06-20 09:56:10 +0900

    Day 4
    express server, router, middleware

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Hi");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleLinstening = () => {
+  console.log("Listening on : http://localhost:4000");
+};
+
+const handleHome = (req, res) => {
+  res.send("Hi I'm from Home");
+};
+
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+};
+
+const testMiddleware = (req, res, next) => {
+  console.log("I'm a middleware");
+  next();
+};
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleLinstening);
