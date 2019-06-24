a2ca943d4e8176289ec00f14efdefe18cb7c3f40
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-06-25 01:02:16 +0900

    Day 8
    View(pug), template ( global / local ) variable

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
+import routes from "./routes";
 import globalRouter from "./Routers/globalRouter";
 import videoRouter from "./Routers/videoRouter";
 import userRouter from "./Routers/userRouter";
+import { localsMiddleware } from "./middleware";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
-app.use(`/`, globalRouter);
-app.use(`/users`, userRouter);
-app.use(`/videos`, videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
