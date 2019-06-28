8203f9230d2e4a66a76e7756ef963ab10ebd63e5
KimsPractice <tpghksdldia@naver.com>
2019-06-28 14:59:43 +0900

    Day 11~12
    Pug(mixin) Controller

--- README.md
+++ README.md
@@ -9,3 +9,16 @@
 ## break Day
 
 - Day 6, 7 -weekend (6/22, 6/23)
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 32445,
+    title: `Video awesome`,
+    description: `This is something I love`,
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 456156,
+      name: "Kimspractice",
+      email: "tpghksdldia@naver.com"
+    }
+  },
+  {
+    id: 12177,
+    title: `Video super`,
+    description: `This is something I love`,
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 873542,
+      name: "Kimspractice",
+      email: "tpghksdldia@naver.com"
+    }
+  },
+  {
+    id: 35423,
+    title: `Video nice`,
+    description: `This is something I love`,
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 8724,
+      name: "Kimspractice",
+      email: "tpghksdldia@naver.com"
+    }
+  },
+  {
+    id: 54356,
+    title: `Video wow`,
+    description: `This is something I love`,
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 12314,
+      name: "Kimspractice",
+      email: "tpghksdldia@naver.com"
+    }
+  }
+];
